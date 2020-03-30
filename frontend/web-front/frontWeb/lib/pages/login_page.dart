4e2c8be2fbe6e3178debc0b2aef8c48207e6e58b
Jhonata Fernandes <jhfernandes.oliveira@gmail.com>
2020-03-30 13:24:02 -0300

    reiniciando projeto

---
+++ frontend/web-front/frontWeb/lib/pages/login_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+        @override
+      Widget build(BuildContext context) {
+    return _body();
+  }
+
+  _body(){
+    return Container(color: Colors.blue,)
+
+  } 
+}
